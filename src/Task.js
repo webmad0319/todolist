a5215dffbcc74150f601766e927119080fa5b84e
Dani Vicario <danigv.email@gmail.com>
2019-05-02 16:53:14 +0200

    improvements on task

--- src/Task.js
+++ src/Task.js
@@ -8,10 +8,6 @@ class Task extends React.Component {
         this.props.onTaskStatusChange(this.props.idx - 1)
     }
 
-    componentDidMount(){
-        console.log("Hola, montada una task")
-    }
-
     render() {
         return (
             <div className={"task " + (this.props.done ? 'done' : null)}>
@@ -19,8 +15,6 @@ class Task extends React.Component {
                 <input type="checkbox" onClick={() => this.setTaskStatus()}></input>
                 <span>{this.props.taskStr}</span>
                 <span className="date">{this.props.creationTimestamp}</span>
-                {/* <span></span> */}
-                {/* <span>{this.props.done}</span> */}
             </div>
         )
     }
